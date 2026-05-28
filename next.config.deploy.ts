ddcef89a9e3cf1aa2b011ad62b939937b660ffaa
priyansh19 <priyansh19@users.noreply.github.com>
2026-05-28 20:46:16 +0000

    deploy: b1d0e629b3bb959ebfdb87ee1599ed48c0954148

--- next.config.deploy.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-const nextConfig: NextConfig = {
-  output: "export",
-  distDir: "deploy",
-  images: { unoptimized: true },
-};
-export default nextConfig;
